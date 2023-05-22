09da6a2be04f4a88c5f9896b7585a09cb09e9cf4
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-05-23 00:19:51 +0700

    homepage and productpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// @page
+import HomePage from "./Modules/Home";
+import ProductPage from "./Modules/Product/";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product" element={<ProductPage />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
