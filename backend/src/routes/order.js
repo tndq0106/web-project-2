94973457aafba433a59cc81806bd624a606a1948
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-05-23 15:04:03 +0700

    fix k can check token

--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -4,45 +4,21 @@ const { authJwt } = require("../app/middleware");
 const OrderController = require("../app/controllers/OrderController");
 
 // Get list order client
-router.post(
-  "/get-list-order-client",
-  [authJwt.verifyToken],
-  OrderController.getListOrderClient
-);
+router.post("/get-list-order-client", OrderController.getListOrderClient);
 
 // Create order
-router.post(
-  "/create-order",
-  [authJwt.verifyToken],
-  OrderController.createOrder
-);
+router.post("/create-order", OrderController.createOrder);
 
 // Get list order
-router.post(
-  "/get-list-order",
-  [authJwt.verifyToken],
-  OrderController.getListOrder
-);
+router.post("/get-list-order", OrderController.getListOrder);
 
 // Get detail order
-router.get(
-  "/get-detail-order/:id",
-  [authJwt.verifyToken],
-  OrderController.getDetailOrder
-);
+router.get("/get-detail-order/:id", OrderController.getDetailOrder);
 
 // Delete detail order
-router.delete(
-  "/delete-detail-order/:id",
-  [authJwt.verifyToken],
-  OrderController.deleteDetailOrder
-);
+router.delete("/delete-detail-order/:id", OrderController.deleteDetailOrder);
 
 // Update detail order
-router.put(
-  "/update-detail-order/:id",
-  [authJwt.verifyToken],
-  OrderController.updateOrder
-);
+router.put("/update-detail-order/:id", OrderController.updateOrder);
 
 module.exports = router;
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -6,31 +6,23 @@ const ProductsController = require("../app/controllers/ProductsController");
 
 // Admin
 
-// create products
-router.post("/create", [authJwt.verifyToken], ProductsController.create);
+router.post("/create", ProductsController.create);
 
 // post list producst with pagination
 router.post(
   "/listProducts",
-  [authJwt.verifyToken],
+
   ProductsController.getListWithPaginate
 );
 
 // get detail products
-router.get("/detail/:id", [authJwt.verifyToken], ProductsController.getDetail);
+router.get("/detail/:id", ProductsController.getDetail);
 
 // update products
-router.put("/update/:id", [authJwt.verifyToken], ProductsController.update);
+router.put("/update/:id", ProductsController.update);
 
 // delete products
-router.delete("/delete/:id", [authJwt.verifyToken], ProductsController.delete);
-
-// // search product
-// router.post(
-//   "/search-product",
-//   [authJwt.verifyToken],
-//   ProductsController.searchProduct
-// );
+router.delete("/delete/:id", ProductsController.delete);
 
 // End
 
